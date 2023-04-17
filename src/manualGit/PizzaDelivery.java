f5273f0d2f9cf295760643c1f2176b1b20fe0396
alserx <alvaro.srnr@gmail.com>
2023-04-17 16:52:36 +0200

    Initial commit

---
+++ src/manualGit/PizzaDelivery.java
@@ -0,0 +1,26 @@
+package manualGit;
+
+public class PizzaDelivery {
+	private int numberOfLateDeliveries;
+	
+	// ...
+
+	int getRating() {
+
+		return moreThanFiveLateDeliveries() ? 2 : 1;
+
+	}
+
+	boolean moreThanFiveLateDeliveries() {
+
+		return numberOfLateDeliveries > 5;
+
+	}
+
+	double potentialEnergy(double mass, double height) {
+
+		return mass * height * 9.81;
+
+	}
+
+}
