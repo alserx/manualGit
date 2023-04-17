8dc062450f03525d853be68a080fb8c68b1f8535
alserx <alvaro.srnr@gmail.com>
2023-04-17 17:30:07 +0200

    getRating method refactored

--- src/manualGit/PizzaDelivery.java
+++ src/manualGit/PizzaDelivery.java
@@ -8,13 +8,7 @@ public class PizzaDelivery {
 
 	int getRating() {
 
-		return moreThanFiveLateDeliveries() ? 2 : 1;
-
-	}
-
-	boolean moreThanFiveLateDeliveries() {
-
-		return numberOfLateDeliveries > 5;
+		return numberOfLateDeliveries > 5 ? 2 : 1;
 
 	}
 
